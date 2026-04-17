68c3ba8c623911f5a0f71985408c2d60fd6cfed9
shahjamanabhuiyanshahrin-prog <shahjamanabhuiyanshahrin@gmail.com>
2026-04-17 15:01:15 +0600

    Update a1_task1.dart

--- lib/a1_task1.dart
+++ lib/a1_task1.dart
@@ -5,7 +5,7 @@ class Task1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("My Info"), 
+        title: Text("Shah Jamana"), 
       ),
       body: Center(
         child: Container(
